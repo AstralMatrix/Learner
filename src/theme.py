22562bda92a0a352f99041ab3b03855e63625f1f
Thomas Westpfal <tomwestpfal@gmail.com>
2020-10-20 20:36:55 -0400

    improved how colors were set, and added documentation

--- src/theme.py
+++ src/theme.py
@@ -1,20 +1,17 @@
 from enum import Enum
+from settings import Settings
+from typing import List
 
-
-DEFAULT_THEME_COLOR = ('#F0F0F0', '#D0D0D0', '#FFFFFF', '#000000', '#008000', '#B22222')
 
 class Theme(object):
 
-    def __init__(self, theme_colors: tuple):
-        theme_colors = DEFAULT_THEME_COLOR
-        self.__colors: dict = {
-            ThemeColor.MAIN_COLOR: theme_colors[0],
-            ThemeColor.SECONDARY_COLOR: theme_colors[1],
-            ThemeColor.BACK_COLOR: theme_colors[2],
-            ThemeColor.TEXT_COLOR: theme_colors[3],
-            ThemeColor.CORRECT_COLOR: theme_colors[4],
-            ThemeColor.INCORRECT_COLOR: theme_colors[5]
-        }
+
+    def __init__(self):
+        self.__colors: dict = None  # Store each color for the theme.
+        self.update_colors()  # Set the colors upon loading.
+
+        # Stores tkinter form items to their respective color grouping
+        # so that they are colored properly.
         self.__item_groups: dict = {
             ThemeGroup.MAIN_GROUP: [],
             ThemeGroup.BUTTON_GROUP: [],
@@ -23,53 +20,80 @@ class Theme(object):
         }
 
 
+    def update_colors(self) -> None:
+        '''Sets the current theme colors from the colors saved in the settings.'''
+        theme_colors: List[str] = Settings.theme_colors
+        self.__colors = {
+            ThemeColor.MAIN_COLOR: theme_colors[0],
+            ThemeColor.SECONDARY_COLOR: theme_colors[1],
+            ThemeColor.BACK_COLOR: theme_colors[2],
+            ThemeColor.TEXT_COLOR: theme_colors[3],
+            ThemeColor.CORRECT_COLOR: theme_colors[4],
+            ThemeColor.INCORRECT_COLOR: theme_colors[5]
+        }
+
+
     def add_to_group(self, item, group: 'ThemeGroup') -> None:
+        '''Adds a tkinter form element to a color grouping.
+
+            Args:
+                item: The tkinter element to be grouped.
+                group (ThemeGroup): The group the object belongs to.
+        '''
         self.__item_groups[group].append(item)
 
 
     def set_theme_color(self) -> None:
+        '''Sets the normal theme coloring to all grouped elements.
+
+            Each group is colored specially to fit the theme. This is also
+            to reset the color after the color has been changed to a grading
+            color.
+        '''
+        # Iterate through all the grouped elements, applying the specific
+        # coloring required for each individual group.
         for key in self.__item_groups.keys():
             for elem in self.__item_groups[key]:
-
                 if key is ThemeGroup.MAIN_GROUP:
                     elem.config(background=self.__colors[ThemeColor.MAIN_COLOR])
-
                 elif key is ThemeGroup.BUTTON_GROUP:
                     elem.config(background=self.__colors[ThemeColor.SECONDARY_COLOR])
                     elem.config(foreground=self.__colors[ThemeColor.TEXT_COLOR])
-
                 elif key is ThemeGroup.LABEL_GROUP:
                     elem.config(background=self.__colors[ThemeColor.MAIN_COLOR])
                     elem.config(foreground=self.__colors[ThemeColor.TEXT_COLOR])
-
                 elif key is ThemeGroup.TEXT_BOX_GROUP:
                     elem.config(background=self.__colors[ThemeColor.BACK_COLOR])
                     elem.config(foreground=self.__colors[ThemeColor.TEXT_COLOR])                
 
 
     def set_correct_color(self) -> None:
+        '''Sets the 'correct' grading color to the elements that display it.'''
+        # Iterate through all the grouped elements, applying the specific
+        # coloring required for each individual group.
         for key in self.__item_groups.keys():
             for elem in self.__item_groups[key]:
-
                 if key is ThemeGroup.MAIN_GROUP:
                     elem.config(background=self.__colors[ThemeColor.CORRECT_COLOR])
-
                 elif key is ThemeGroup.LABEL_GROUP:
                     elem.config(background=self.__colors[ThemeColor.CORRECT_COLOR])
 
 
     def set_incorrect_color(self) -> None:
+        '''Sets the 'incorrect' grading color to the elements that display it.'''
+        # Iterate through all the grouped elements, applying the specific
+        # coloring required for each individual group.
         for key in self.__item_groups.keys():
             for elem in self.__item_groups[key]:
-
                 if key is ThemeGroup.MAIN_GROUP:
                     elem.config(background=self.__colors[ThemeColor.INCORRECT_COLOR])
-
                 elif key is ThemeGroup.LABEL_GROUP:
                     elem.config(background=self.__colors[ThemeColor.INCORRECT_COLOR])
 
 
 class ThemeGroup(Enum):
+    '''Enum for the different coloring groups that any given element can
+        be colored as in the given theme.'''
     MAIN_GROUP: str = "main_group"
     BUTTON_GROUP: str = "button_group"
     LABEL_GROUP: str = "label_group"
@@ -77,6 +101,7 @@ class ThemeGroup(Enum):
 
 
 class ThemeColor(Enum):
+    '''Enum for each of the different colors in the given theme.'''
     MAIN_COLOR: str = "main_color"
     SECONDARY_COLOR: str = "secondary_color"
     BACK_COLOR: str = "back_color"
