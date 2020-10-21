18bc6364e06f7b1145b93f79f7e272628722147b
Thomas Westpfal <tomwestpfal@gmail.com>
2020-10-20 23:04:01 -0400

    added documentation

--- src/grader.py
+++ src/grader.py
@@ -4,34 +4,65 @@ from quiz_queue import QuizQueue
 from typing import List
 
 
+# Data object displaying that an error occured.
 ERROR_D_OBJ: DataObject = DataObject([["ERROR"]])
 
 
 class Grader(object):
+    '''A simple object to be used by the main form to grade and
+        display questions (data objects) to help remove the QuizQueue
+        overhead. Utilizes the QuizQueue for storing and getting data objects'''
 
     def __init__(self, file_paths: List[str]):
+        # Load in all data objects from the given files.
         self.__raw_data: List[DataObject] = FileReader.read_files(file_paths)
 
-        if self.__raw_data is None:
+        if self.__raw_data is None:  # Display error data object if loading failed.
             self.__raw_data = [ERROR_D_OBJ]
 
+        # Create a quiz queue to randomize the elements.
         self.__elements: QuizQueue = QuizQueue(self.__raw_data)
         self.__curr_element: DataObject = self.__elements.next()
 
 
     def get_display_question(self, segment_idx: int) -> str:
+        '''Get the segment of a data object. The given segment represents
+            the question to be displayed.
+            
+            Args:
+                segment_idx (int): The index of the desired segment.
+
+            Returns:
+                str: The string representation of the given segment. 
+        '''
         return self.__curr_element.segment_str(segment_idx)
 
 
     def get_display_answer(self) -> str:
+        '''Display the entire data object. This represents the answer to
+            the question to be displayed.
+
+            Returns:
+                str: The string representation of the given data object. 
+        '''
         return str(self.__curr_element)
 
 
     def next(self) -> None:
+        '''Move on to the next data object from the quiz queue.'''
         self.__curr_element = self.__elements.next()
 
     
     def check(self, input_str: str) -> bool:
+        '''Check if the input string is correct for the current data object.
+            Recycle the data object back into the quiz queue if it is not.
+
+            Args:
+                input_str (str): The string to be checked.
+
+            Returns:
+                bool: If the input string was correct or not.
+        '''
         is_correct: bool = self.__curr_element.check(input_str)
         # If the answer is wrong add it back in near the front of the
         # queue, so it can be given again.
